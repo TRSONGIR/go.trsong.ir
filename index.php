fa0d8f8305a7da7dd738c9f3f0fcece7c8f70cb1
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-07-28 19:36:32 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 $tr="http://sib-design.ir/sibmusic/demo2/?p=";
 $id= $_GET['url'];
-$url= $tr.$id;
 if($id != '' ){
+$url= $tr.$id;
 header("Location: $url");
 exit();
 }else{
-header("Location: http://doman.ir");
+header("Location: http://trsong.ir");
 exit();
 }
 ?>
